ca245f29beb9cd1cf39f5e989b132d4ad131ee1b
Pratyush Panda <pratyushpanda530@gmail.com>
2025-06-15 19:23:40 +0530

    notejs req edited prepared for the deployment

--- backend/SRC/controllers/notesController.js
+++ backend/SRC/controllers/notesController.js
@@ -1,6 +1,6 @@
 import Note from "../models/note.js";
 
-export async function getAllNotes(req, res) {
+export async function getAllNotes(_, res) {
   // res.status(200).send("you just fetched the notes");
   try {
     const notes = await Note.find().sort({ createdAt: -1 }); //newest first
